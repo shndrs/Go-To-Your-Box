bfbc3cfebfd7a864e11b19134c41e82aa07f0348
Sahand Raeisi <sahandraeisi@yahoo.com>
2019-01-19 15:23:43 +0330

    Create extensions file for ViewController

---
+++ Go To Your Box/Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  Extensions.swift
+//  Go To Your Box
+//
+//  Created by Sahand Raeisi on 1/19/19.
+//  Copyright © 2019 Sahand Raeisi. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+var width:CGFloat = 0.2
+var height:CGFloat = 1
+var length:CGFloat = 1
+
+var doorLength: CGFloat = 0.3
+
+
+func createBox(isDoor:Bool) -> SCNNode {
+    let node = SCNNode()
+    
+    // Add first Box
+    
+    let firstBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
+    let firstBoxNode = SCNNode(geometry: firstBox)
+    firstBoxNode.renderingOrder = 200
+    
+    node.addChildNode(firstBoxNode)
+    
+    // Add Masked Box
+    
+    let maskedBox = SCNBox(width: width, height: height, length: isDoor ? doorLength : length, chamferRadius: 0)
+    maskedBox.firstMaterial?.diffuse.contents = UIColor.lightGray
+    maskedBox.firstMaterial?.transparency = 0.00001
+    let maskedBoxNode = SCNNode(geometry: maskedBox)
+    maskedBoxNode.renderingOrder = 100
+    maskedBoxNode.position = SCNVector3(width, 0, 0)
+    
+    return node
+}
