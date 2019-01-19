1abc8ab48521fab1fe5c04c02706583dc295efa0
Sahand Raeisi <sahandraeisi@yahoo.com>
2019-01-19 15:28:29 +0330

    Added walls of the box to the ViewController in method setUpScene

--- Go To Your Box/ViewController.swift
+++ Go To Your Box/ViewController.swift
@@ -46,6 +46,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    
+    
+    
+    
+    func setUpScene() {
+        let node = SCNNode()
+        
+        let leftWall = createBox(isDoor: false)
+        let rightWall = createBox(isDoor: false)
+        let topWall = createBox(isDoor: false)
+        let bottomWall = createBox(isDoor: false)
+        
+        let leftDoorSide = createBox(isDoor: true)
+        let rightDoorSide = createBox(isDoor: true)
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
 
     // MARK: - ARSCNViewDelegate
     
